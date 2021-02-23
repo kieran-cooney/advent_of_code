ecb131bb98e7fd6d7f779821ed672676c48a7a86
Kieran Cooney <kieran.cooney@hotmail.com>
2021-02-23 20:44:00 +0000

    2015 day 5 part 2

--- 2015/5/solution.py
+++ 2015/5/solution.py
@@ -1,61 +1,78 @@
-
 import sys
 import re
+from operator import add
+from functools import reduce
 
-from typing import List, Iterable
+from typing import List, Iterable, Pattern
 
-VOWELS = 'aeiou'
-# Regex to check for repeated character
-REPEATED_CHAR_PATTERN = re.compile(r"([a-z])\1{1}")
-BAD_STRINGS = ['ab', 'cd', 'pq', 'xy']
+# Regex is type representing a compiled regex.
+Regex = Pattern[str]
+Regexes = Iterable[Pattern[str]]
 
+# List of lists for parts 1 and 2
+REGEX_PATTERNS = [
+    [r"([aeiou].*){3}", r"([a-z])\1{1}", r"^((?!ab|cd|pq|xy).)*$"],
+    [r"([a-z]{2}).*\1", r"([a-z])[a-z]\1"]
+]
 
-def read_input(filepath: str) => Iterable[str]:
+def read_input(filepath: str) -> Iterable[str]:
     with open(filepath) as f:
         for line in f:
             yield line.strip()
 
 
-def contains_vowels(string: str, num_vowels: int =3) -> bool:
-    string_vowels = [e for e in string if e in VOWELS]
-    return (len(string_vowels) >= num_vowels)
+def regexp_match(string: str, regex: Regex) -> bool:
+    return bool(regex.search(string))
 
 
-def contains_repeated_char(string: str) -> bool:
-    return bool(REPEATED_CHAR_PATTERN.search(string))
+def good_string(string: str, regexes: Regexes,
+                verbose: bool = False) -> bool:
+    """Checks if string matches against each regex in regexes"""
+    results = [regexp_match(string, r) for r in regexes]
 
-
-def contains_bad_string(
-    string: str,
-    bad_strings: List[str] = BAD_STRINGS) -> bool:
-    return any(s in string for s in bad_strings)
-
-
-def good_string(string: str, verbose: bool = False) -> bool:
-    test_results = [
-        contains_vowels(string),
-        contains_repeated_char(string),
-        not contains_bad_string(string)
-    ]
-
-    result = all(test_results)
+    result = all(results)
 
     if verbose:
         print("Results for {}".format(string))
-        print("Sub tests: {}".format(test_results))
+        print("Sub tests: {}".format(results))
         print("Final result: {}".format(result))
 
     return result
 
 
-def count_good_strings(strings: Iterable[str]) -> int:
-    return sum(1 for s in strings if good_string(s))
+def count_good_strings(strings: Iterable[str],
+                        regex_lists: List[Regexes],
+                        verbose: bool=False) -> List[int]:
+    """
+    Checks how many of strings match and returns a list of these counts for
+    each list of regexes in regex_lists
+    """
+    def list_sum(l1, l2):
+        """
+        Helper function to add two lists pair-wise
+        returning a list of the same length
+        """
+        return map(add, l1, l2)
+
+    # Iterator of lists of good_string results for each regex_list
+    good_strings = (
+        [good_string(s, r, verbose) for r in regex_lists]
+        for s in strings
+    )
+
+    return reduce(list_sum, good_strings)
 
 
 def main(filepath: str):
     strings = read_input(filepath)
-    num_good_strings = count_good_strings(strings)
-    print("Part 1: {} good strings found".format(num_good_strings))
+
+    # Compile regexes here so we only have to do it once.
+    compiled_regexes = [[re.compile(s) for s in l ] for l in REGEX_PATTERNS]
+
+    num_good_strings = count_good_strings(strings, compiled_regexes)
+
+    for i, ans in enumerate(num_good_strings, start=1):
+        print("Part {}: {} good strings found".format(i, ans))
 
 
 if __name__=='__main__':
--- 2015/5/tests.py
+++ 2015/5/tests.py
@@ -1,25 +1,43 @@
-from solution import good_string
+import re
+from solution import good_string, REGEX_PATTERNS
+
+compiled_regexes = [[re.compile(s) for s in l ] for l in REGEX_PATTERNS]
 
 test_cases = [
-    ('ugknbfddgicrmopn', True),
-    ('aaa', True),
-    ('jchzalrnumimnmhp', False),
-    ('haegwjzuvuyypxyu', False),
-    ('dvszwmarrgswjxmb', False)
+    [
+        ('ugknbfddgicrmopn', True),
+        ('aaa', True),
+        ('jchzalrnumimnmhp', False),
+        ('haegwjzuvuyypxyu', False),
+        ('dvszwmarrgswjxmb', False)
+    ],
+    [
+        ('qjhvhtzxzqqjkmpb', True),
+        ('xxyxx', True),
+        ('uurcxstgmygtbstg', False),
+        ('ieodomkazucvgmuy', False),
+    ]
 ]
 
-num_correct = 0
+def test(cases, regexes, part):
+    num_correct = 0
 
-for test, answer in test_cases:
-    print("Expected answer for {}: {}".format(test, answer))
-    result = good_string(test, True)
+    for test, answer in cases:
+        print("Expected answer for {}: {}".format(test, answer))
+        result = good_string(test, regexes, True)
 
-    if result == answer:
-        print("Right answer!")
-        num_correct += 1
-    else:
-        print("Wrong answer!")
-    
-    print("")
+        if result == answer:
+            print("Right answer!")
+            num_correct += 1
+        else:
+            print("Wrong answer!")
+        
+        print("")
 
-print('Number of cases right: {}/{}'.format(num_correct, len(test_cases)))
+    print(
+        'Number of cases right for part {}: {}/{}'
+        .format(part, num_correct, len(cases))
+    )
+
+for i, (c, r) in enumerate(zip(test_cases, compiled_regexes), start=1):
+    test(c, r, i)
